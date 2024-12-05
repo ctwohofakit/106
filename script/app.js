0b5fd2baad59927a4a03bbf4dc75b6d1dfac95b2
ctwohofakit <ctwohofa@gmail.com>
2024-12-04 19:51:46 -0800

    SecondClass

--- script/app.js
+++ script/app.js
@@ -1,11 +1,18 @@
-function sayGoodbye(){
-    console.log("Goodbye");
+function saveTask(){
+    console.log("saving task");
+}
+
+
+function init(){
+    console.log("task manager");
+    //load data
+
+
+
+    //hook the events
+    $("#btnSave").click(saveTask);
+
 
 }
 
-function sayHello(){
-    console.log("Hello");
-    sayGoodbye ();
-}
-    window.onload = sayGoodbye;
-    
+window.onload=init;
